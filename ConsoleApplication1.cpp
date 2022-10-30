8f14ab50ed411a11503f4846ae5eec1840f36307
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-30 17:21:56 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿// Чертоусова Ветвления
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a >= c && a >= b) cout << a;
+	if (b >= a && b >= c) cout << b;
+	if (c >= a && c >= b) cout << c;
+}
